8474ac4ac7254659e2ddddf2af93f8974452afec
Justin Joplin <joplin.justin@gmail.com>
2023-11-02 19:10:12 -0500

    fix: use underscores in tag names

--- tags.tf
+++ tags.tf
@@ -3,9 +3,9 @@ resource "digitalocean_tag" "lfs258" {
 }
 
 resource "digitalocean_tag" "controller" {
-  name = "lfs258-controller"
+  name = "lfs258_controller"
 }
 
 resource "digitalocean_tag" "worker" {
-  name = "lfs258-worker"
+  name = "lfs258_worker"
 }
